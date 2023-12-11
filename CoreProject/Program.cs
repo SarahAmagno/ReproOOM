c6a27e4d37637b83c1d49f5c152b5d1b326f0bc5
Sarah Benecke <sbe@amagno.de>
2023-12-11 11:57:50 +0100

    Create project for OOM repro

---
+++ CoreProject/Program.cs
@@ -0,0 +1,27 @@
+﻿using GemBox.Spreadsheet;
+
+SpreadsheetInfo.SetLicense("INSERT LICENSE KEY HERE");
+
+const string outputDirectory = @"..\..\..\OutputFiles";
+const string inputPath = "InputFile.xlsx";
+
+for (var i = 0; i < 15; i++)
+{
+    var spreadSheet = ExcelFile.Load(inputPath, new XlsxLoadOptions
+    {
+        StreamingMode = LoadStreamingMode.Read
+    });
+    
+    var pages = spreadSheet.GetPaginator(new PaginatorOptions { SelectionType = SelectionType.EntireFile }).Pages;
+        
+    for (var index = 0; index < pages.Count; index++)
+    {
+        var fileName = $"Spreadsheet-{i}_Page-{index}.bmp";
+            
+        var page = pages[index];
+            
+        using var stream = new FileStream(Path.Combine(outputDirectory, fileName), FileMode.Create);
+            
+        page.Save(stream, new ImageSaveOptions(ImageSaveFormat.Bmp));
+    }
+}
